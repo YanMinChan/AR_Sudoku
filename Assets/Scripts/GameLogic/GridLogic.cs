76d151bf2ecffc60eeb4332e6bca18bae1508be8
Yan Min <yc89@hw.ac.uk>
2025-06-09 12:42:25 +0100

    Update grid logic

--- Assets/Scripts/GameLogic/GridLogic.cs
+++ Assets/Scripts/GameLogic/GridLogic.cs
@@ -8,11 +8,52 @@ using System.Threading.Tasks;
 public class GridLogic
 {
     // Instantiate the cells
-    CellLogic[,] cells = new CellLogic[9, 9];
+    CellLogic[,] cells;
     
-    public GridLogic(int[] puzzle, int[] solution)
+    public GridLogic()
     {
-        // Initialising the subgrid
+        this.cells = new CellLogic[9, 9];
+        //// Initialising the subgrid
+        //int[] subgrid = {1, 1, 1, 2, 2, 2, 3, 3, 3
+        //    , 1, 1, 1, 2, 2, 2, 3, 3, 3
+        //    , 1, 1, 1, 2, 2, 2, 3, 3, 3
+        //    , 4, 4, 4, 5, 5, 5, 6, 6, 6
+        //    , 4, 4, 4, 5, 5, 5, 6, 6, 6
+        //    , 4, 4, 4, 5, 5, 5, 6, 6, 6
+        //    , 7, 7, 7, 8, 8, 8, 9, 9, 9
+        //    , 7, 7, 7, 8, 8, 8, 9, 9, 9
+        //    , 7, 7, 7, 8, 8, 8, 9, 9, 9};
+
+        //// Fill the cells with given puzzle and solution
+        //for (int i = 0; i < 9; i++)
+        //{
+        //    for (int j = 0; j < 9; j++)
+        //    {
+        //        //Console.WriteLine(i*9 + j);
+        //        this.cells[i, j] = new CellLogic(puzzle[i * 9 + j], solution[i * 9 + j], subgrid[i * 9 + j]);
+        //    }
+        //}
+    }
+
+    public CellLogic[,] Cells
+    {
+        get { return this.cells; }
+    }
+
+    // Generate the grid with a random puzzle from the list
+    public CellLogic[,] GenerateGrid(string filePath)
+    {
+        PuzzleReader reader = new PuzzleReader();
+        Random rand = new Random();
+
+        // Load puzzle from file path and store them
+        reader.ReadCSV(filePath);
+        List<int[]> puz = reader.Puzzle;
+        List<int[]> sol = reader.Solution;
+
+        // Choose a random puzzle and load it
+        int r = rand.Next(puz.Count);
+
         int[] subgrid = {1, 1, 1, 2, 2, 2, 3, 3, 3
             , 1, 1, 1, 2, 2, 2, 3, 3, 3
             , 1, 1, 1, 2, 2, 2, 3, 3, 3
@@ -23,14 +64,14 @@ public class GridLogic
             , 7, 7, 7, 8, 8, 8, 9, 9, 9
             , 7, 7, 7, 8, 8, 8, 9, 9, 9};
 
-        // Fill the cells with given puzzle and solution
         for (int i = 0; i < 9; i++)
         {
             for (int j = 0; j < 9; j++)
             {
-                //Console.WriteLine(i*9 + j);
-                this.cells[i, j] = new CellLogic(puzzle[i * 9 + j], solution[i * 9 + j], subgrid[i * 9 + j]);
+                this.cells[i, j] = new CellLogic(puz[r][i * 9 + j], sol[r][i * 9 + j], subgrid[i * 9 + j]);
             }
         }
+
+        return this.cells;
     }
 }
--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -8,8 +8,16 @@ public class GridController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        PuzzleReader reader = new PuzzleReader();
-        reader.ReadCSV("./Assets/Resoruces/sudoku.csv");
+        string filePath = "./Assets/Resources/sudoku.csv"; // path of the dataset
+        GridLogic grid = new GridLogic();
+        CellLogic[,] cells = grid.GenerateGrid(filePath);
+        if (cells != null)
+        {
+            Debug.Log("Cell loaded: " + cells.Length);
+        } else
+        {
+            Debug.Log("AAAAAAAA");
+        }
     }
 
     // Update is called once per frame
